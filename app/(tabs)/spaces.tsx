2add03823b4fed7ccb14e30e18950cec395b89c7
Sanu Kumar <sanu23kumar@gmail.com>
2025-03-04 23:30:11 +0530

    remove unused code

--- app/(tabs)/spaces.tsx
+++ app/(tabs)/spaces.tsx
@@ -1,11 +1,8 @@
 import { StyleSheet } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
-import { useThemeColor } from "@/hooks/useThemeColor";
 import ScreenContainer from "@/components/ScreenContainer";
 
-export default function HomeScreen() {
-  const backgroundColor = useThemeColor({}, "background");
-
+export default function TabTwoScreen() {
   return (
     <ScreenContainer>
       <ThemedText>Hello</ThemedText>
