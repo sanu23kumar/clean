4ec3e41378fd80d0951988eac510edf11c18f910
Sanu Kumar <sanu23kumar@gmail.com>
2025-03-04 23:30:26 +0530

    add screen container

---
+++ components/ScreenContainer.tsx
@@ -0,0 +1,27 @@
+import { Image, StyleSheet, Platform, SafeAreaView } from "react-native";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+export default function ScreenContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const backgroundColor = useThemeColor({}, "background");
+
+  return (
+    <SafeAreaView style={[{ backgroundColor }, styles.parent]}>
+      {children}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  parent: {
+    flex: 1,
+  },
+});
